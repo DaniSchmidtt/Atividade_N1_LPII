7b6669429d715b8b34424bd4cd9dcc25f075b334
caisoc200 <56918423+caisoc200@users.noreply.github.com>
2020-09-04 21:03:58 -0300

    Commit 02(encapsulamento e biblioteca)

--- AtividadeCaixa/src/Models/ItemEstoque.java
+++ AtividadeCaixa/src/Models/ItemEstoque.java
@@ -15,4 +15,171 @@ public class ItemEstoque {
     private double ValorPagoCompra;
     private double ValorMargem100;
     private double PrecoSugerido;
+    /**
+     * @return the CodigoDoItem
+     */
+    public int getCodigoDoItem() {
+        return CodigoDoItem;
+    }
+
+    /**
+     * @param CodigoDoItem the CodigoDoItem to set
+     */
+    public void setCodigoDoItem(int CodigoDoItem) {
+        this.CodigoDoItem = CodigoDoItem;
+    }
+
+    /**
+     * @return the DataDeEntrada
+     */
+    public Date getDataDeEntrada() {
+        return DataDeEntrada;
+    }
+
+    /**
+     * @param DataDeEntrada the DataDeEntrada to set
+     */
+    public void setDataDeEntrada(Date DataDeEntrada) {
+        this.DataDeEntrada = DataDeEntrada;
+    }
+
+    /**
+     * @return the LocalDaCompra
+     */
+    public String getLocalDaCompra() {
+        return LocalDaCompra;
+    }
+
+    /**
+     * @param LocalDaCompra the LocalDaCompra to set
+     */
+    public void setLocalDaCompra(String LocalDaCompra) {
+        this.LocalDaCompra = LocalDaCompra;
+    }
+
+    /**
+     * @return the Tipo
+     */
+    public String getTipo() {
+        return Tipo;
+    }
+
+    /**
+     * @param Tipo the Tipo to set
+     */
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    /**
+     * @return the Marca
+     */
+    public String getMarca() {
+        return Marca;
+    }
+
+    /**
+     * @param Marca the Marca to set
+     */
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    /**
+     * @return the Caracteristicas
+     */
+    public String getCaracteristicas() {
+        return Caracteristicas;
+    }
+
+    /**
+     * @param Caracteristicas the Caracteristicas to set
+     */
+    public void setCaracteristicas(String Caracteristicas) {
+        this.Caracteristicas = Caracteristicas;
+    }
+
+    /**
+     * @return the Tamanho
+     */
+    public EnumTamanho getTamanho() {
+        return Tamanho;
+    }
+
+    /**
+     * @param Tamanho the Tamanho to set
+     */
+    public void setTamanho(EnumTamanho Tamanho) {
+        this.Tamanho = Tamanho;
+    }
+
+    /**
+     * @return the Cor
+     */
+    public EnumCor getCor() {
+        return Cor;
+    }
+
+    /**
+     * @param Cor the Cor to set
+     */
+    public void setCor(EnumCor Cor) {
+        this.Cor = Cor;
+    }
+
+    /**
+     * @return the ValorEtiquetaCompra
+     */
+    public double getValorEtiquetaCompra() {
+        return ValorEtiquetaCompra;
+    }
+
+    /**
+     * @param ValorEtiquetaCompra the ValorEtiquetaCompra to set
+     */
+    public void setValorEtiquetaCompra(double ValorEtiquetaCompra) {
+        this.ValorEtiquetaCompra = ValorEtiquetaCompra;
+    }
+
+    /**
+     * @return the ValorPagoCompra
+     */
+    public double getValorPagoCompra() {
+        return ValorPagoCompra;
+    }
+
+    /**
+     * @param ValorPagoCompra the ValorPagoCompra to set
+     */
+    public void setValorPagoCompra(double ValorPagoCompra) {
+        this.ValorPagoCompra = ValorPagoCompra;
+    }
+
+    /**
+     * @return the ValorMargem100
+     */
+    public double getValorMargem100() {
+        return ValorMargem100;
+    }
+
+    /**
+     * @param ValorMargem100 the ValorMargem100 to set
+     */
+    public void setValorMargem100(double ValorMargem100) {
+        this.ValorMargem100 = ValorMargem100;
+    }
+
+    /**
+     * @return the PrecoSugerido
+     */
+    public double getPrecoSugerido() {
+        return PrecoSugerido;
+    }
+
+    /**
+     * @param PrecoSugerido the PrecoSugerido to set
+     */
+    public void setPrecoSugerido(double PrecoSugerido) {
+        this.PrecoSugerido = PrecoSugerido;
+    }
 }
